0fbe838ed510735f3165941ead2a26cc8ab02810
Matthias De Fre <matthias.defre.y7363@student.hogent.be>
2017-02-21 15:22:56 +0100

    Made getPlayerData + implementation

--- src/domain/DomainController.java
+++ src/domain/DomainController.java
@@ -9,21 +9,23 @@ public class DomainController {
 		throw new UnsupportedOperationException();
 	}
 
-	public String[][] getPlayerDate() {
-		String[][] dataAndDeck = new String[2][];
-                String[] playerData = new String[3];
-                String[][] playerDeck = new String[currentUser.getDeck().length][];
-                playerData[0] = currentUser.getName();
-                playerData[1] = currentUser.getDateOfBirth().toString();
-                playerData[2] = String.valueOf(currentUser.getCredits());
-                dataAndDeck[0] = playerData;
-                for (int i = 0; i < currentUser.getDeck().length - 1; i++)
-              {      
-             playerDeck[i][0] =   currentUser.getDeck()[i].getType();
-             playerDeck[i][1] =   String.valueOf(currentUser.getDeck()[i].getValue());        
-            }
-                dataAndDeck[1] = playerDeck;
-                return dataAndDeck;
+	public String[][] getPlayerDate()
+    {
+        String[][] dataAndDeck = new String[currentUser.getDeck().length + 1][];
+        String[] playerData = new String[3];
+        String[] cardData = new String[2];
+        playerData[0] = currentUser.getName();
+        playerData[1] = currentUser.getDateOfBirth().toString();
+        playerData[2] = String.valueOf(currentUser.getCredits());
+        dataAndDeck[0] = playerData;
+        for (int i = 0; i < currentUser.getDeck().length - 1; i++)
+        {
+            cardData[0] = currentUser.getDeck()[i].getType();
+            cardData[1] = String.valueOf(currentUser.getDeck()[i].getValue());
+            dataAndDeck[i + 1] = cardData;
+        }
+
+        return dataAndDeck;
 	}
 
 	public DomainController() {
