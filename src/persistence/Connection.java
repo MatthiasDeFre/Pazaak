f15b0b7fdb4ce4db8595eec6b616f93efc40c081
JelleGeers <JelleGeers@192.168.0.240>
2017-02-21 16:20:59 +0100

    implementation of Datasource

---
+++ src/persistence/Connection.java
@@ -0,0 +1,9 @@
+
+package persistence;
+
+
+public class Connection {
+    public static final String JDBC_URL="jdbc:mysql://ID222177_g07.db.webhosting.be?user=ID222177_g07&password=Wec4twagwib?";
+    
+    
+}
