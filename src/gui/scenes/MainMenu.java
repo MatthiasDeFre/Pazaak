d9f26d8d188e33ed5ce102a8fd880360d1572bfa
Gerrit De Vriese <gerrit.devriese.y7370@student.hogent.be>
2017-04-18 10:08:48 +0200

    Gui

---
+++ src/gui/scenes/MainMenu.java
@@ -0,0 +1,16 @@
+package gui.scenes;
+
+import javafx.scene.layout.BorderPane;
+import domain.DomainController;
+
+
+
+public class MainMenu extends BorderPane
+{
+     private final DomeinController dc;
+    
+     public void panel {}
+    
+    
+    
+}
---
+++ src/startUp/StartUpGUI.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package startUp;
+
+import domain.DomainController;
+import gui.scenes.LanguageSelection;
+import gui.scenes.MainMenu;
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author User
+ */
+public class StartUpGUI
+{
+    public static void main(String[] args)
+    {
+        DomainController dc = new DomainController();
+        Scene scene = new Scene(new LanguageSelection(dc), 420, 69);
+        
+        
+//        if (gebruiker nog geen taal heeft geselcteerd)
+//        {
+//            Scene scene = new Scene(new LanguageSelection(dc), 420, 69);
+//        }
+//        else
+//        {
+//              Scene scene = new Scene(new MainMenu(dc), 420, 69);  
+//        }        
+        
+        scene.getStylesheets().add("/gui/styles.css");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
