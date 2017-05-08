e09a8f7a8f54b2c5b502050e8c0c979d9aa9f78d
Matthias De Fre <matthias.defre.y7363@student.hogent.be>
2017-05-08 19:38:17 +0200

    Test

--- src/domain/GameBoard.java
+++ src/domain/GameBoard.java
@@ -13,46 +13,67 @@ import java.util.List;
  * @author Matthias
  */
 public class GameBoard {
+
     private List<Card> gameBoardCards = new ArrayList<>();
     private boolean frozen;
-    
+
     /**
      * Method to play a Card and add this to the gameboard (gameBoardCards)
+     *
      * @param card Instance of Card that needs to be added
      */
     public void playCard(Card card)
     {
+        String cardType = card.getType();
+        if(cardType.equals("D"))
+        {
+                if (gameBoardCards.size() < 0)
+                {
+                    card.setValue(gameBoardCards.get(gameBoardCards.size() - 1).getTrueValue());
+                }
+        } else if(cardType.contains("&")){
+            for (Card gameBoardCard : gameBoardCards)
+            {
+                //if(gameBoardCard.getValue() ==)
+            }
+        }
         gameBoardCards.add(card);
     }
-     
+
     /**
      * Method to freeze the board
-     */  
-    public void freezeBoard() {
+     */
+    public void freezeBoard()
+    {
         this.frozen = true;
     }
-    public boolean getFrozen() {
+
+    public boolean getFrozen()
+    {
         return this.frozen;
     }
-    
-    public List<Card> getGameBoardCards() {
+
+    public List<Card> getGameBoardCards()
+    {
         return this.gameBoardCards;
     }
-    
+
     /**
      * <pre> Method to calculate the score of the gameboard and return it
-     * {@link domain.Card#getTrueValue() }is used instead of {@link domain.Card#getValue()} 
+     * {@link domain.Card#getTrueValue() }is used instead of {@link domain.Card#getValue()}
      * because for calculations the signed representation should be used</pre>
+     *
      * @return The score of the gameboard
      */
-    public int calculateGameBoardScore() {
-       
-        int score=0;
+    public int calculateGameBoardScore()
+    {
+
+        int score = 0;
         for (Card gameBoardCard : gameBoardCards)
         {
             score += gameBoardCard.getTrueValue();
         }
-       return score;
+        return score;
     }
-    
+
 }
